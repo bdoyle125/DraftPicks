8445ddc6ba9c7e2ded2670099bca5e70f7b1faba
Bryce Doyle <bdoyle125@gmail.com>
2023-08-22 11:06:47 -0500

    Add number input validation

--- draftOrder.java
+++ draftOrder.java
@@ -4,14 +4,9 @@ public class draftOrder {
 
     private static void oneUserStandardDraft() {
         Scanner scan = new Scanner(System.in);
-        System.out.println("How many rounds is your draft?");
-        int rounds = scan.nextInt();
-
-        System.out.println("How many participants are in your draft?");
-        int participants = scan.nextInt();
-
-        System.out.println("What is the number of your first pick?");
-        int originalPick = scan.nextInt();
+        int rounds = inputValidation("How many rounds is your draft?");
+        int participants = inputValidation("How many participants are in your draft?");
+        int originalPick = inputValidation("What is the number of your first pick?");
 
         scan.close();
 
@@ -30,17 +25,12 @@ public class draftOrder {
 
     private static void oneUserSnakeDraft() {
         Scanner scan = new Scanner(System.in);
-        System.out.println("How many rounds is your draft?");
-        int rounds = scan.nextInt();
-
-        System.out.println("How many participants are in your draft?");
-        int participants = scan.nextInt();
-
-        System.out.println("What is the number of your first pick?");
-        int originalPick = scan.nextInt();
+        int rounds = inputValidation("How many rounds is your draft?");
+        int participants = inputValidation("How many participants are in your draft?");
+        int originalPick = inputValidation("What is the number of your first pick?");
 
         scan.close();
-        
+
         int userPick;
         int totalPicks = 0;
         for (int i = 1; i <= rounds; i++) {
@@ -59,27 +49,47 @@ public class draftOrder {
                 }
             }
         }
-    } 
-  public static void main(String[] args) {
-    Scanner scan = new Scanner(System.in);
-
-    System.out.println("This program tells you all your draft picks in a draft");
-    System.out.println("What kind of draft is it? Type either Standard or Snake.");
-    String draftType = scan.nextLine();
-
-    switch(draftType) {
-        case "Standard":
-            oneUserStandardDraft();
-            break;
-        case "Snake":
-            oneUserSnakeDraft();
-            break;
-        default:
-            System.out.println("That was not one of the options. Please restart the program to try again.");
-            break;
     }
 
-    scan.close();
-    
-  }
+    public static int inputValidation(String prompt) {
+        int value = 0;
+        Scanner scan = new Scanner(System.in);
+        boolean flag = false;
+
+        System.out.println(prompt);
+        // Making sure the input is an integer.
+        while (!(flag)) {
+            if (scan.hasNextInt()) {
+                value = scan.nextInt();
+                flag = true;
+            } else {
+                scan.next();
+                System.out.println("Input value must be a non-decimal number.");
+            }
+        }
+        return value;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("This program tells you all your draft picks in a draft");
+        System.out.println("What kind of draft is it? Type either Standard or Snake.");
+        String draftType = scan.nextLine();
+
+        switch (draftType) {
+            case "Standard":
+                oneUserStandardDraft();
+                break;
+            case "Snake":
+                oneUserSnakeDraft();
+                break;
+            default:
+                System.out.println("That was not one of the options. Please restart the program to try again.");
+                break;
+        }
+
+        scan.close();
+
+    }
 }
